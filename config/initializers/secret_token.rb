a792106ac17859807a9ea82dead91ef43b066c9b
Student <student@CA-iMac-10.local>
2012-08-28 14:53:06 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testpdf::Application.config.secret_token = '3cbdebc64316ff9b87736e3c22b6ed1a6cbd9ce719d9a0dcfbad1a9929cc180f123c04f5171c9db3b807eac00f6ad8200030d807b0c620eeba515e6c1017c54d'
